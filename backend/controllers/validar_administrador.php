fe064a473fad0fde2965a8c1bc4cba339a9045d1
yasiri2005 <giraldoyasiri@gmail.com>
2025-12-02 11:08:27 -0500

    login

---
+++ backend/controllers/validar_administrador.php
@@ -0,0 +1,35 @@
+<?php
+require_once "../models/MySQL.php";
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $usuario = $_POST["usuario"];
+    $password = $_POST["password"];
+
+    $db = new MySQL();
+    $conn = $db->conectar();
+
+    $usuario = mysqli_real_escape_string($conn, $usuario);
+    $password = mysqli_real_escape_string($conn, $password);
+
+    $sql = "SELECT * FROM tbl_administradores 
+            WHERE usuario_administrador = '$usuario'
+            AND password_administrador = '$password'";
+
+    $resultado = $db->efectuarConsulta($sql);
+
+    if ($resultado && mysqli_num_rows($resultado) > 0) {
+
+        $_SESSION["admin"] = $usuario;
+
+        header("Location: ../../index.php");
+        exit();
+
+    } else {
+        header("Location: ../views/login_administrador.php?error=1");
+        exit();
+    }
+
+}
+?>
---
+++ backend/controllers/validar_aprendiz.php
@@ -0,0 +1,31 @@
+<?php
+require_once "../models/MySQL.php";
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $ficha = $_POST["ficha"];
+    $usuario = $_POST["usuario_jugador"];
+
+    $db = new MySQL();
+    $conn = $db->conectar();
+
+    $sql = "SELECT * FROM tbl_jugadores
+            WHERE ficha_jugador = '$ficha'
+            AND usuario_jugador = '$usuario'";
+
+    $resultado = $db->efectuarConsulta($sql);
+
+    if (mysqli_num_rows($resultado) > 0) {
+
+        $_SESSION["aprendiz"] = $usuario;
+        header("Location: ../../index.php");
+        exit();
+
+    } else {
+
+        header("Location: ../views/login_aprendiz.php");
+        exit();
+    }
+}
+?>
